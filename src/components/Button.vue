541c0ff45f6f1d1605835a2cea1d6118dcec3e59
Bedirhan Celayir <44666921+rokasta12@users.noreply.github.com>
2022-08-09 22:43:28 +0300

    fix: spacing

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,97 +1,80 @@
 <template>
-   <div class="button-wrapper">
-        <button 
-        :class="buttonType"
-        :disabled="disabled"
-        @click="handleButton($event)"
-        
-        >
-        {{buttonText}}
-        </button>
-       
-   </div>
+	<div class="button-wrapper">
+		<button
+			:class="buttonType"
+			:disabled="disabled"
+			@click="handleButton($event)"
+		>
+			{{ buttonText }}
+		</button>
+	</div>
 </template>
 
-
 <script>
-
-
 export default {
+	props: {
+		buttonText: {
+			type: String,
+			default: () => "Primary Button"
+		},
 
+		buttonType: {
+			type: String,
+			default: () => "primary"
+		},
 
-    props : {
-
-        buttonText: {
-            type: String,
-            default: ()=> 'Primary Button'
-        },
-
-        buttonType: {
-            type: String,
-            default: ()=> "primary"
-        },
-            
-        disabled: {
-            type: Boolean,
-            default: false
-            
-        },
-        icon: {
-            type: String,
-            default: ''
-        },
-
-
-    },
-    methods :{
-        handleButton(e){
-            this.$emit('click', e);
-        }
-    }
-    
-}
+		disabled: {
+			type: Boolean,
+			default: false
+		},
+		icon: {
+			type: String,
+			default: ""
+		}
+	},
+	methods: {
+		handleButton(e) {
+			this.$emit("click", e);
+		}
+	}
+};
 </script>
 
-
 <style lang="scss" scoped>
+button {
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	padding: 12px 16px;
+	border-radius: 8px;
+	border: none;
 
-button{
-display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: center;
-padding: 12px 16px ;
-border-radius: 8px;
-border: none;
-
-&[disabled]{
-cursor: not-allowed;
-opacity: 0.38;
+	&[disabled] {
+		cursor: not-allowed;
+		opacity: 0.38;
+	}
+	&.primary {
+		background-color: #ff6000;
+		color: #ffffff;
+	}
+	&.primary:hover {
+		background-color: #bf4800;
+	}
+	&.variant {
+		background-color: #ffefe5;
+		color: #ff6000;
+	}
+	&.variant:hover {
+		background-color: #ffbf99;
+		color: #bf4800;
+	}
+	&.secondary {
+		background-color: #6b83fa;
+		color: #ffffff;
+	}
+	&.secondary:hover {
+		background-color: #5365bf;
+	}
 }
-&.primary{
-background-color: #FF6000;
-color: #FFFFFF;
-}
-&.primary:hover{
-background-color: #BF4800;
-}
-&.variant{
-background-color: #FFEFE5;
-color: #FF6000;
-}
-&.variant:hover{
-background-color: #FFBF99;
-color: #BF4800;
-}
-&.secondary{
-background-color: #6B83FA;
-color: #FFFFFF;
-}
-&.secondary:hover{
-background-color: #5365BF;
-
-}
-}
-
-
 </style>
