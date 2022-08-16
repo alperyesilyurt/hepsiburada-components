2e39d4306c0c8dafbe8aa8cea41f2ce8909758e2
Alper Yesilyurt <alperesilyurt@hotmail.com>
2022-08-16 13:07:20 +0300

    switch added

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,44 +1,35 @@
 <template>
-   <div>
-    <label @click="changeStatus" class="switch">
-  <input   type="checkbox" > 
-  <span class="slider round"></span>
-</label>
-   
-   </div> 
+  <div>
+    <label @click="switchFunc" class="switch">
+      <input type="checkbox" />
+      <span class="slider round"></span>
+    </label>
+  </div>
 </template>
 <script>
 export default {
-    props: {
-      statusFromParent : {
-      type: Object,
-      default: {},
-    }
+  props: {
+    switchValue: {
+      type: String,
+      default: "",
+    },
+  },
 
-    }
-    ,
-
-
-
-    methods : {
-        changeStatus(){
-            this.$emit("statusFromChild",this.statusFromParent)
-        }
-    }
-    
-   
-    
-}
+  methods: {
+    switchFunc(e) {
+      this.$emit("switchFunc", e);
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 .switch {
   position: absolute;
   width: 56px;
   height: 32px;
 }
-.switch input { 
+.switch input {
   opacity: 0;
   width: 0;
   height: 0;
@@ -51,11 +42,11 @@ export default {
   left: 0;
   right: -2px;
   bottom: 0;
-  background: #F0F0F0;
-  border: 2px solid #DADADA;
+  background: #f0f0f0;
+  border: 2px solid #dadada;
   border-radius: 32px;
-  -webkit-transition: .4s;
-  transition: .4s;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
 }
 
 .slider:before {
@@ -66,28 +57,22 @@ export default {
   left: 2px;
   bottom: 2px;
   background-color: #006907;
-  -webkit-transition: .4s;
-  transition: .4s;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
 }
 
-
-
-
 input:checked + .slider {
-
-background: #E5FFE7;
-border: 2px solid #008A09;
-border-radius: 32px;
+  background: #e5ffe7;
+  border: 2px solid #008a09;
+  border-radius: 32px;
 }
 
 input + .slider:before {
- background: #8C8C8C;
+  background: #8c8c8c;
 }
 input:checked + .slider:before {
- background: #006907;
-
+  background: #006907;
 }
-
 
 input:checked + .slider:before {
   -webkit-transform: translateX(26px);
@@ -95,11 +80,9 @@ input:checked + .slider:before {
   transform: translateX(26px);
 }
 
-
 /* Rounded sliders */
 .slider.round {
   border-radius: 32px;
-  
 }
 
 .slider.round:before {
--- src/pages/SwitchPage.vue
+++ src/pages/SwitchPage.vue
@@ -1,16 +1,29 @@
 <template>
-    <compSwitch></compSwitch>
+  <div class="switch">
+    <compSwitch 
+    @switchFunc="func1"
+    
+    
+    />
+  </div>
 </template>
 
 <script>
-import compSwitch from "../components/Switch.vue"
+import compSwitch from "../components/Switch.vue";
 export default {
-    components:{
-        compSwitch,
-    }
-}
+  components: {
+    compSwitch,
+  },
+   methods: {
+    func1() {
+      console.log("func1");
+    },}
+};
 </script>
 
 <style lang="scss" scoped>
 
+.switch{
+    margin: 50px;
+}
 </style>
