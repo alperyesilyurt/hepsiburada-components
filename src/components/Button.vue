85ebedc8779209cbced3d01def539571be8ce680
Alper Yesilyurt <alperesilyurt@hotmail.com>
2022-08-17 15:56:47 +0300

    Update Button.vue

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="btn">
     <button
-      class="btn-content"
-      :class="`btn-${buttonType}`"
+      :class="`btn-content btn-content-${buttonType}`"
       :disabled="disabled"
       @click="handleButton($event)"
     >
       {{ buttonText }}
-
       <img :src="require(`../assets/${icon}`)" />
     </button>
   </div>
@@ -56,45 +54,44 @@ export default {
     border-radius: 8px;
     border: none;
     gap: 10px;
-  }
-
-  &-primary {
-    background-color: #ff6000;
-    color: #ffffff;
-  }
-  &-primary:hover {
-    background-color: #bf4800;
-  }
-  &-variant {
-    background-color: #ffefe5;
-    color: #ff6000;
-  }
-  &-variant:hover:not([disabled]) {
-    background-color: #ffbf99;
-    color: #bf4800;
-  }
-  &-secondary {
-    background-color: #6b83fa;
-    color: #ffffff;
-  }
-  &-secondary:hover {
-    background-color: #5365bf;
-  }
-  &-bordered {
-    padding: 10px 14px;
-    background-color: transparent;
-    border: 2px solid #dadada;
-    color: #212121;
-  }
-  &-bordered:hover {
-    background-color: #f0f0f0;
-  }
-  &-ghost {
-    background-color: transparent;
-    color: #212121;
-  }
-  &-ghost:hover {
-    background-color: #f0f0f0;
+    &-primary {
+      background-color: #ff6000;
+      color: #ffffff;
+      &:hover:not([disabled]) {
+        background-color: #bf4800;
+      }
+    }
+    &-variant {
+      background-color: #ffefe5;
+      color: #ff6000;
+      &:hover:not([disabled]) {
+        background-color: #ffbf99;
+        color: #bf4800;
+      }
+    }
+    &-secondary {
+      background-color: #6b83fa;
+      color: #ffffff;
+      &:hover:not([disabled]) {
+        background-color: #5365bf;
+      }
+    }
+    &-bordered {
+      padding: 10px 14px;
+      background-color: transparent;
+      border: 2px solid #dadada;
+      color: #212121;
+      &:hover:not([disabled]) {
+        background-color: #f0f0f0;
+      }
+    }
+    &-ghost {
+      background-color: transparent;
+      color: #212121;
+      &:hover:not([disabled]) {
+        background-color: #f0f0f0;
+      }
+    }
   }
   &-content[disabled] {
     cursor: not-allowed;
